cbfeb12b15f2fae58501cbb2083d6e26664deb45
NataliFed <fnadnepr@gmail.com>
2022-06-06 15:04:51 +0200

    Add posts

--- src/features/home/HomeFeature.tsx
+++ src/features/home/HomeFeature.tsx
@@ -8,36 +8,34 @@ export const HomeFeature = () => {
 
    useEffect(() => {
        fetch('https://dummyjson.com/posts?limit=10')
-       .then(res => res.json())
+       .then((res) => res.json())
        .then((data) => {
-           console.log(data);
            setPosts(data.posts);
        });  
-   })
-       console.log('Hello Home Feature')
+   }, [])
+      
             
-    return (<div>{posts.map((posts: any)=><div key={posts.id}>
-            <div css={css`
-                display: flex;
-                flex-direction: column;
-                columns: 3;
-                justify-content: space-between;
-            `}>
-                <div css={css`
+    return (<div   css={css`
+        display: flex;
+        flex-direction: column;
+        columns: 3;
+        justify-content: space-between;
+        `}>{posts.map((posts: any)=>(
+            <div key={posts.id} css={css`
                     max-width: 200px;
                     margin-bottom: 10px;
                     border: 2px solid #a3a2a2e1;
                     border-radius: 5px;
                     padding: 15px;
                 `}>
-                    <p css={css`
+                <p css={css`
                         text-transform: uppercase;
                         text-align: center;
-                    `}>{posts.title}</p>
-                    <div>{posts.body}</div>
-                </div>
+                    `}>
+                        {posts.title}
+                </p>
+                <div>{posts.body}</div>
             </div>
-        </div>
-        )}
+        ))}
     </div>)
 }
