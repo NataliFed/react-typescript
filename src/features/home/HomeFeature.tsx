54b17b7b7a2d99bf0523738e2700cba375d9680b
NataliFed <fnadnepr@gmail.com>
2022-06-06 14:19:15 +0200

    add body

--- src/features/home/HomeFeature.tsx
+++ src/features/home/HomeFeature.tsx
@@ -1,2 +1,43 @@
-export const HomeFeature = () => 
-    <h1>Hello</h1>
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { useEffect, useState } from "react";
+
+export const HomeFeature = () => {
+   
+   const [posts, setPosts] = useState([]);
+
+   useEffect(() => {
+       fetch('https://dummyjson.com/posts?limit=10')
+       .then(res => res.json())
+       .then((data) => {
+           console.log(data, 'data');
+           setPosts(data.posts);
+       });  
+   })
+       console.log('Hello Home Feature')
+            
+    return (<div>{posts.map((posts: any)=><div key={posts.id}>
+            <div css={css`
+                display: flex;
+                flex-direction: column;
+                columns: 3;
+                justify-content: space-between;
+            `}>
+                <div css={css`
+                    max-width: 200px;
+                    margin-bottom: 10px;
+                    border: 2px solid #a3a2a2e1;
+                    border-radius: 5px;
+                    padding: 15px;
+                `}>
+                    <p css={css`
+                        text-transform: uppercase;
+                        text-align: center;
+                    `}>{posts.title}</p>
+                    <div>{posts.body}</div>
+                </div>
+            </div>
+        </div>
+        )}
+    </div>)
+}
--- src/features/page/PageFeatures.tsx
+++ src/features/page/PageFeatures.tsx
@@ -1,2 +1,10 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
 export const PageFeature = ({children}: any) => 
-    <div>{children}</div>
+    <div css={css`
+        max-width: 800px;
+        margin: auto;
+        box-shadow: 1px 1px 6px 3px #2a29297d;
+        padding: 15px;
+    `}>{children}</div>
